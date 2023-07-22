4fa34a594b6a9790f0b8f0866433beddebbcc9e0
Saumen Das <123894562+saumen2000@users.noreply.github.com>
2023-07-22 16:01:04 +0000

    2 file variable done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 19036   
+// const keyword variable accountId mai value fixed hoo gaya hai
+
+let accountEmail = "saumenj19036@gmail.com"
+
+
+var accountPassword = 123456
+var accountState
+
+console.table([accountId,accountEmail,accountPassword]);
+
+accountEmail = "john12@gmail.com"
+accountPassword = 785452
+accountCity = "Mumbai"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
